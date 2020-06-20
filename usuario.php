2659c8ab424c1d91b0678001dd16b7d9be859b32
maninha75 <63216145+maninha75@users.noreply.github.com>
2020-06-19 23:28:54 -0300

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -14,9 +14,6 @@ class Usuario {
     return $this->senha;
   }
 
-  //public function getRepet() {
-    //return $this->repete;
-  //}
   public function setlogin($login) {
   $this->login = $login;
   }
@@ -24,39 +21,19 @@ class Usuario {
   public function setSenha($senha) {
     $this->senha = $senha;
     }
-  //public function setRepete($repete) {
-      //$this->repete = $repete;
-      //}
     
   public function __construct($login, $senha){
       $this->login = $login;
       $this->senha = $senha;
-      //$this->repete = $repete;
-}
+ }
 
 public function validarSenha($senha, $login){
-
-            
-            // Estamos comparando uma instância(objeto) com uma string
-            if($login("cida") && $senha("123456789-09")){
+           if($login("cida") && $senha("123456789-09")){
                 echo "Usuário logado com sucesso.";
             }else{
                 echo "Login ou senha inválidos!";
-            }
-        }
-      
+           }
+     }  
 }
 
-
-
-  //  if ($senha == $repete) {
-//echo "Senhas conferem";
-//}
-//else {
-//echo "Senhas não conferem";
-//}
-//}
-//}
-//$Usuario = new Usuario("Aparecida",12345, 12345);
-//echo $Usuario->validarSenha();
 ?>
